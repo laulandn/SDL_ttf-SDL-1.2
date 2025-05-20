bc95a83cecc4d694e7914f9af28add4abfb05001
Nick Lauland <laulandn@gmail.com>
2025-05-20 16:19:35 -0500

    update and reorg of externals halfway

--- SDL_ttf/autogen.sh
+++ SDL_ttf/autogen.sh
@@ -1,1 +1,11 @@
-#!/bin/sh#set -eaclocal -I acincludeautomake --foreign --include-deps --add-missing --copyautoconf#./configure $*echo "Now you are ready to run ./configure"
+#!/bin/sh
+#
+
+set -e
+
+aclocal -I acinclude
+automake --foreign --include-deps --add-missing --copy
+autoconf
+
+#./configure $*
+echo "Now you are ready to run ./configure"
--- SDL_ttf/external/zlib-1.2.8/contrib/delphi/ZLibConst.pas
+++ SDL_ttf/external/zlib-1.2.8/contrib/delphi/ZLibConst.pas
@@ -1,1 +1,11 @@
-unit ZLibConst;interfaceresourcestring  sTargetBufferTooSmall = 'ZLib error: target buffer may be too small';  sInvalidStreamOp = 'Invalid stream operation';implementationend.
+unit ZLibConst;
+
+interface
+
+resourcestring
+  sTargetBufferTooSmall = 'ZLib error: target buffer may be too small';
+  sInvalidStreamOp = 'Invalid stream operation';
+
+implementation
+
+end.
